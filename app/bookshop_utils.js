d11d796be86371c7a9c4ec111b5ff33fafc97ebe
Samantha Burke <samanthaburke.uk@gmail.com>
2019-11-26 15:10:11 +0000

    passes all tests

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,7 +1,40 @@
 function addVAT(price) {
-    return price + (0.2 * price);
+    //return price + (0.2 * price);
+    return price * 1.2;
+}
+
+function getFullName (firstName, surname) {
+    return firstName + ' ' + surname ;
+}
+
+function makeHalfPrice (price) {
+    return price / 2
+}
+
+function countBooks(arr) {
+    return arr.length;
+}
+
+function isInStock (book) {
+    if (book.quantity > 0) {
+        return true;
+    } else {
+       return false;
+    }        
+}
+
+function getTotalOrderPrice(price, quantity) {
+    return (price * quantity) * 1.2;
 }
 
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
+
+/* Lots of formatting issues with this code, cut and pasted from Windows to Mac. 
+Must remember to format as code snippet in future! */
--- test/utils.test.js
+++ test/utils.test.js
@@ -4,8 +4,55 @@ describe("utils.addVAT", () => {
   test("returns 36 when passed 30", () => {
     expect(utils.addVAT(30)).toBe(36);
   });
-  
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
+  });
+  test('returns 118.8 when passed 99', () => {
+      expect (utils.addVAT(99)).toBe(118.8);
+  });
+});
+
+describe("utils.getFullName", () => {
+  test("returns Susan Jason when passed Susan and Jason", () => {
+    expect(utils.getFullName("Susan", "Jason")).toBe("Susan Jason");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 50 when price is 100", () => {
+    expect(utils.makeHalfPrice(100)).toBe(50);
+  });
+});
+describe('utils.countBooks', () =>{
+  test( " return the amounts of books in the array", () => {
+    expect (utils.countBooks(['Harry Potter', 'Hunger Games','LOTR'])).toBe(3);
+  });
+});
+
+describe('utils.isInStock', () => {
+  test('If book in stock, true is returned', () => {
+    const book = {
+      title: 'Harry Potter and the Philospher Stone',
+      author: ' J K Rowling', 
+      yearOfPublication: 1999,
+      quantity: 6 }; 
+
+      expect(utils.isInStock (book)).toBe(true);
+    }); 
+
+  test('If book is out of stock, false is returned', () => {
+    const book = {
+      title: 'Northern Lights',
+      author: 'Phillip Pullman',
+      yearOfPublication: 1995,
+      quantity: 0 };
+
+      expect(utils.isInStock (book)).toBe(false);
+  });    
+});
+
+describe('utils.getTotalOrderPrice', () => {
+  test ('If item £20, total is £24 including VAT', () => {
+    expect(utils.getTotalOrderPrice(20,1)).toBe(24);
   });
 });
